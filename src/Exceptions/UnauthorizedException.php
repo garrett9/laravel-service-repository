4532946dfc738f8c31aa18539c4e95864ba02d3a
garrett <garrettshevach@gmail.com>
2016-12-26 21:37:53 -0500

    Added new exception.

---
+++ src/Exceptions/UnauthorizedException.php
@@ -0,0 +1,13 @@
+<?php
+namespace Garrett9\LaravelServiceRepository\Exceptions;
+
+/**
+ * An exception to indicate that the action is not authorized to be performed.
+ *
+ * @author garrettshevach@gmail.com
+ *        
+ */
+class UnauthorizedException extends \Exception
+{
+
+}
